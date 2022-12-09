cf7478fb269cc982ffa5a473fb7e0cb281262fb6
parnuphun <parnuphun1598@gmail.com>
2022-12-09 22:19:45 +0700

    SweetAlert config

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,24 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import routes from './plugin/routes'
 
-// v-calendar
-import 'v-calendar/dist/style.css';
-import VCalendar from 'v-calendar';
-
-
-//tailwind
+// tailwind css
 import './assets/style/tailwind.css'
 
 // Vuetify
 import vuetify from './plugin/vuetify/vuetify'
+
+// v-calendar
+import VCalendar from 'v-calendar'
+import 'v-calendar/dist/style.css'
+
+// sweet alert 2
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 
 createApp(App)
     .use(VCalendar)
     .use(vuetify)
     .use(routes)
+    .use(VueSweetalert2)
     .mount('#app')
