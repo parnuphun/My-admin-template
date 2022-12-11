9478b96b6ccfa56c4db58280106148c3015dc332
parnuphun <parnuphun1598@gmail.com>
2022-12-11 15:37:48 +0700

    update DataTable

--- src/components/components/DataTable.vue
+++ src/components/components/DataTable.vue
@@ -3,7 +3,8 @@ import { onMounted } from 'vue';
 
     export interface Column {
         key: string,
-        label?: string
+        label?: string,
+        class?:string
     }
 
     interface Data {
@@ -12,34 +13,83 @@ import { onMounted } from 'vue';
 
     const props = defineProps<{
         columns: Column[],
-        data: Data[]
+        data: Data[],
+        total: number,
+        loading?:boolean,
+        perPage: number,
+        page: number,
+        sizes?: number[],
     }>()
 
-    onMounted(()=>{
+    const emit = defineEmits<{
+        (event: 'update:per-page' , size:number):void
+        (event: 'update:page' , size:number):void
+    }>()
 
-    })
-
+    function onChangePage(number:number){
+        const nextNumber = props.page + number
+        if(nextNumber <= 0){
+            return
+        }
+        emit('update:page',nextNumber )
+    }
 </script>
 
 <template>
 
 
-    <table class="">
+    <table class="rounded-xl shadow-xl w-full">
         <thead class="bg-cyan-200 text-black">
             <tr>
-                <th class="py-2 px-3 first:rounded-tl-xl last:rounded-tr-xl"
+                <th :class="column.class"
+                    class="py-2 px-3 first:rounded-tl-xl last:rounded-tr-xl"
                     v-for="column of props.columns" :key="column.key">
                     {{column.label}}
                 </th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="(item , i ) of props.data" :key="i">
-                <td class="px-3 py-1"
+            <tr v-if="props.loading">
+                <td :colspan="props.columns.length"
+                    class="bg-white w-full text-center py-1"> Loading... </td>
+            </tr>
+            <tr v-else v-for="(item , i ) of props.data" :key="i"
+                class="even:bg-slate-100">
+                <td class="py-2 px-3" :class="column.class"
                     v-for="column of props.columns" :key="column.key" >
                     {{ item[column.key] }}
                 </td>
             </tr>
         </tbody>
     </table>
+
+    <div class="flex mt-3 justify-between items-center">
+        <div class="space-x-3">
+            <button
+                @click="onChangePage(-1)"
+                class="py-2 px-3 bg-blue-500 hover:bg-blue-600 text-white rounded-md transition focus:ring focus:ring-blue-300">
+                Previos
+            </button>
+            <button
+                @click="onChangePage(+1)"
+                class="py-2 px-3 bg-blue-500 hover:bg-blue-600 text-white rounded-md transition focus:ring focus:ring-blue-300">
+                Next
+            </button>
+            <span> Current Page {{ props.page }}</span>
+        </div>
+        <div class="space-x-2">
+            <span>
+                {{ props.perPage * (props.page - 1 ) }} - {{ props.perPage * props.page }} รายการ จาก {{ props.total.toLocaleString() }} รายการ
+            </span>
+
+            <select
+                @input="emit('update:per-page', +($event.target as HTMLSelectElement).value)"
+                class="border-2 border-solid text-center px-3 rounded-lg">
+                <option :value="size" v-for="(size,i) of props.sizes || [5,10,15,25,50,100]" :selected="props.perPage === size">
+                    {{ size }}
+                </option>
+            </select>
+        </div>
+    </div>
+
 </template>
--- src/views/test/Test_DataTable.vue
+++ src/views/test/Test_DataTable.vue
@@ -1,37 +1,72 @@
 <script setup lang="ts">
 import AdminNavigationBar from '../../components/layout/AdminNavigationBar.vue';
-import { onMounted , ref ,toRaw } from 'vue'
+import { onMounted , watch , ref ,toRaw } from 'vue'
 import DataTable , {Column} from '../../components/components/DataTable.vue';
 import moment from 'moment'
 import apiRPTS from '../../services/apiRPTS';
+import { reactive } from 'vue';
 
 const api = new apiRPTS()
 
-const columns : Column[] = [
-    { key: 'fullName' , label: 'ชื่อเต็ม'},
-    { key: 'firstName' , label: 'ชื่อจริง'},
-    { key: 'lastName' , label: 'นามสกุล'},
-    { key: 'birthDate' , label: 'อายุ'}
-]
-const data = ref([])
+interface TableOption{
+    columns: Column[],
+    data: any[],
+    total: number,
+    loading : boolean
+}
 
-onMounted(async ()=>{
+const tableOption = reactive<TableOption>({
+    columns : [
+    { key: 'fullName' , label: 'ชื่อเต็ม' , class: "text-left w-96"},
+    { key: 'firstName' , label: 'ชื่อจริง' , class: "text-left" },
+    { key: 'lastName' , label: 'นามสกุล' , class: "text-left"},
+    { key: 'age' , label: 'อายุ' , class: "text-center truncate"},
+    { key: 'birthDate' , label: 'วันเกิด' , class: "text-center truncate"},
+ ],
+    data: [],
+    total: 0,
+    loading: true
+})
 
-    await api.testDataTable().then((res)=>{
-        data.value = res.data.map((name:any)=>{
+const loading = ref(true)
+const page = ref(1)
+const perPage = ref(10)
+
+async function getUser() {
+
+    tableOption.loading = true
+
+    await api.testDataTable(page.value,perPage.value).then((res)=>{
+        const startDate = moment()
+        const data = res.data as {usersData: any , total:number}
+        tableOption.data = data.usersData.map((name:any)=>{
             name.fullName = `${name.firstName} ${name.lastName}`
-            name.birthDate = moment(name.birthDate).format('LL')
+            name.age = startDate.diff(name.birthDate,'years') + ' ปี'
+            name.birthDate = moment(name.birthDate).add(543,'years').format('DD/MM/YYYY')
             return name
         })
+        tableOption.total = data.total
     })
 
+    tableOption.loading = false
+}
 
-})
+onMounted(getUser)
+watch(page,getUser)
+watch(perPage,getUser)
 
 </script>
 
 <template>
     <AdminNavigationBar>
-        <DataTable :columns="columns" :data="data"></DataTable>
+        <DataTable
+            v-bind="tableOption"
+            v-model:per-page="perPage"
+            v-model:page="page">
+
+        </DataTable>
+
+
+        <button @click=""></button>
     </AdminNavigationBar>
 </template>
