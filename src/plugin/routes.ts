a0395fb24e3a39dfae77f6bb3c24fee27e25c02d
parnuphun <parnuphun1598@gmail.com>
2022-12-12 16:29:28 +0700

    fix /#/ in url

--- src/plugin/routes.ts
+++ src/plugin/routes.ts
@@ -1,7 +1,7 @@
-import { createRouter , createWebHashHistory } from 'vue-router'
+import { createRouter , createWebHistory } from 'vue-router'
 
 const router = createRouter({
-    history : createWebHashHistory(),
+    history : createWebHistory(),
     routes: [
         // default redirect
         { path: '/' , component: () => import('../views/finalProject/Main/DashBoard.vue')} ,
@@ -10,11 +10,13 @@ const router = createRouter({
         { path: '/recycle' , component: () => import('../views/finalProject/Main/Recycle.vue')} ,
 
 
+        // check system
+        { path: '/ckeckBackEnd/login' , component:() => import('../views/RPTS_Check_Backend/LoginPage.vue')},
+
         // test component
         { path: '/test_calendar' , component: () => import('../views/test/Test_Calendar.vue')} ,
         { path: '/Test_DataTableManual' , component: () => import('../views/test/Test_DataTableManual.vue')} ,
         { path: '/test_sweetAlert' , component: () => import('../views/test/Test_sweetAlert.vue')} ,
-
 
         // page not found 404
         { path: '/:pathMatch(.*)*' , component: ()=> import ('../views/finalProject/ErrorPage/PageNotFound404.vue')} ,
