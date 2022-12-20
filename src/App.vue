fadff30db6d5aab31d18ca52bf736d15f47018c6
parnuphun <parnuphun1598@gmail.com>
2022-12-20 18:57:20 +0700

    :art: Add css in scrollbar

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,20 @@
     .swal2-container {
         z-index: 20000 !important;
     }
+
+        /* width */
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: #a1a1aa;
+        border-radius: 5px;
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        background: #71717a;
+    }
 </style>
