00ea7480700c558c7a9a3376367fb19b500cfc79
parnuphun <parnuphun1598@gmail.com>
2023-01-02 19:08:27 +0700

    ⚡️ Interface & Store Data

---
+++ src/store/GlobalData.ts
@@ -0,0 +1,13 @@
+import { webSetting } from "./theme/themeData"
+import { reactive, ref } from "vue"
+
+export const isRail = ref<boolean>(true)
+
+export let setting :webSetting = reactive({
+    theme : 'light'
+})
+
+export let userData = ref()
+
+export const credentialData = reactive(JSON.parse(localStorage.getItem('credential')||''))
+
---
+++ src/store/Interface.ts
@@ -0,0 +1,17 @@
+export interface userDataRes {
+    User_Id: number
+    User_Avatar: string
+    User_Usernname: string
+    User_Email: string
+    User_Rmuti_Id: string
+    User_password: string
+    User_Fname: string
+    User_Lname: string
+    User_Phone: string
+    User_AboutMe: string
+    User_Joined_Date: Date
+    User_Email_Confirm: boolean
+    User_Deleted: boolean
+    User_Banned: boolean
+    USER_ROLES: string[]
+}
