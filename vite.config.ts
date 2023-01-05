37d2821474553843509552856fc88d3bd76a3c11
parnuphun <parnuphun1598@gmail.com>
2023-01-05 23:55:44 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server : {
-    proxy : {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+    plugins: [vue()],
+    server : {
+        proxy : {
+        '/api': {
+            // target: 'http://128.199.86.217:4000/api',
+            target: 'http://localhost:4000/api',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+        }
     }
-  }
 })
