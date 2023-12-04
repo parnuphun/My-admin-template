8cd5de28b6ad05390c911252beabdf4b945cb704
parnuphun <81858246+parnuphun@users.noreply.github.com>
2023-12-04 19:49:04 +0700

    admin nav update

--- src/components/layout/AdminNavigationBar.vue
+++ src/components/layout/AdminNavigationBar.vue
@@ -101,7 +101,7 @@ function isGroupOpen() {
 
 <template>
     <VThemeProvider>
-        <VApp>
+        <VApp class="">
             <!-- nav bar -->
             <v-app-bar :elevation="2" color="">
                 <!-- toggle sidebar -->
@@ -109,129 +109,17 @@ function isGroupOpen() {
 
                 <!-- content in nav -->
                 <div class="w-full mr-6 flex flex-row items-center">
-                    <v-app-bar-title color=""> ระบบหลังบ้านโรงเรียน... </v-app-bar-title>
-
-                    <!-- <v-btn icon class="" @click="isAlert = !isAlert" >
-                        <v-icon size="x-large" >mdi-message</v-icon>
-                     </v-btn> -->
+                    <v-app-bar-title color=""> ระบบหลังบ้านโรงเรียนเทศบาลน้ำพองภูริพัฒน์ </v-app-bar-title>
 
                     <v-btn icon class="" @click="fullscreen">
                         <v-icon size="x-large" v-if="isFullScreen">mdi-fullscreen-exit</v-icon>
                         <v-icon size="x-large" v-else="isFullScreen">mdi-fullscreen</v-icon>
                      </v-btn>
 
-                    <!-- message box -->
-                    <!-- <v-menu :close-on-content-click="false" location="bottom">
-                        <template v-slot:activator="{ props }">
-                            <v-btn icon v-bind="props" class="mr-3">
-                                <v-badge content="6" color="red">
-                                    <v-icon >mdi-bell-outline</v-icon>
-                                </v-badge>
-                            </v-btn>
-                        </template>
-
-                        <v-card min-width="400">
-                            <v-divider></v-divider>
-                            <v-list density="compact">
-                                <v-list-item
-                                    title="กล่องข้อความ"
-                                >
-                                </v-list-item>
-                                <v-divider></v-divider>
-
-                                <v-list height="200">
-
-                                    <v-list-item
-                                        width="400"
-                                        title="เสนอโครงการติดตามงานวิจัย"
-                                        subtitle="โครงงานที่เสนอไปได้รับการตอบกลับแล้ว"
-                                    >
-                                        <template v-slot:prepend>
-                                            <v-avatar color="red">
-                                                <v-icon color="white">mdi-text-box-remove</v-icon>
-                                            </v-avatar>
-                                        </template>
-                                    </v-list-item>
-                                    <v-divider></v-divider>
-                                    <v-list-item
-                                        width="400"
-                                        title="ผลประเมินการสอบ"
-                                        subtitle="ผลการสอบโครงการในวันที่ 7 มกราคม 2566"
-                                    >
-                                        <template v-slot:prepend>
-                                            <v-avatar color="green">
-                                                <v-icon color="white">mdi-text-box-check</v-icon>
-                                            </v-avatar>
-                                        </template>
-                                    </v-list-item>
-                                    <v-divider></v-divider>
-                                    <v-list-item
-                                        width="400"
-                                        title="มีวันที่กำหนดการสอบโครงการแล้ว"
-                                        subtitle="กำหนดสอบวันที่ 7 มกราคม 2566"
-                                    >
-                                        <template v-slot:prepend>
-                                            <v-avatar color="orange">
-                                                <v-icon color="white">mdi-calendar-badge</v-icon>
-                                            </v-avatar>
-                                        </template>
-                                    </v-list-item>
-                                    <v-divider></v-divider>
-                                    <v-list-item
-                                        width="400"
-                                        title="ถึงกำหนดวันสอบโครงการแล้ว"
-                                        subtitle="โครงการ ระบบติดตามความก้าวหน้างานวิจัย"
-                                    >
-                                        <template v-slot:prepend>
-                                            <v-avatar color="orange">
-                                                <v-icon color="white">mdi-calendar-alert-outline</v-icon>
-                                            </v-avatar>
-                                        </template>
-                                    </v-list-item>
-                                    <v-divider></v-divider>
-                                    <v-list-item
-                                        width="400"
-                                        title="เอกสารได้รับการตอบกลับจากอาจารย์ที่ปรึกษาแล้ว"
-                                        subtitle="กำหนดสอบวันที่ 7 มกราคม 2566"
-                                    >
-                                        <template v-slot:prepend>
-                                            <v-avatar color="green">
-                                                <v-icon color="white">mdi-text-box-check</v-icon>
-                                            </v-avatar>
-                                        </template>
-                                    </v-list-item>
-                                    <v-divider></v-divider>
-                                    <v-list-item
-                                        width="400"
-                                        title="มีโหัวข้อโครงการที่เสนอเข้ามาใหม่"
-                                        subtitle="หัวข้อโครงการระบบติดตามความก้าวหน้างานวิจัย"
-                                    >
-                                        <template v-slot:prepend>
-                                            <v-avatar color="orange">
-                                                <v-icon color="white">mdi-account-arrow-right</v-icon>
-                                            </v-avatar>
-                                        </template>
-                                    </v-list-item>
-                                </v-list>
-                            </v-list>
-                        </v-card>
-                    </v-menu> -->
                     <!-- user profile -->
                     <v-menu :close-on-content-click="false" location="bottom">
                         <template v-slot:activator="{ props }">
                             <v-btn  v-bind="props" icon>
-                                <!-- character icon -->
-                                <!-- <v-avatar
-                                    color="brown"
-                                    size=""
-                                    >
-                                    <span class="text-h5 p-1">ภน</span>
-                                </v-avatar> -->
-                                <!-- image icon // not work -->
-                                <!-- <v-avatar
-                                :src="'https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133352156-stock-illustration-default-placeholder-profile-icon.jpg'"
-                                    >
-                                </v-avatar> -->
                                 <!-- icon -->
                                 <v-icon size="large">mdi-account-circle-outline</v-icon>
                              </v-btn>
@@ -264,26 +152,10 @@ function isGroupOpen() {
                             <v-divider></v-divider>
                         </v-card>
                     </v-menu>
-
-                    <!-- theme mode button switch -->
-                    <!-- <v-btn-toggle
-                        variant="outlined"
-                        divided>
-                        <v-btn
-                            icon="mdi-brightness-5"
-                            @click="changeTheme('light')"
-                            :active="rentTheme === 'light'">
-                        </v-btn>
-                        <v-btn
-                            icon="mdi-brightness-2"
-                            @click="changeTheme('dark')"
-                            :active="rentTheme === 'dark'">
-                        </v-btn>
-                    </v-btn-toggle> -->
                 </div>
             </v-app-bar>
 
-            <v-progress-linear
+            <!-- <v-progress-linear
                 v-if="true"
                 indeterminate
                 color="primary"
@@ -292,7 +164,7 @@ function isGroupOpen() {
                 height="5"
                 style="z-index:9999;"
                 >
-            </v-progress-linear>
+            </v-progress-linear> -->
 
             <!-- side bar -->
             <v-navigation-drawer
@@ -350,37 +222,18 @@ function isGroupOpen() {
                         </v-list-group>
                     </div>
                 </v-list>
-
-                <!-- test child -->
-
             </v-navigation-drawer>
+
+
             <!-- Content here !!! -->
             <VMain>
-                <v-container fluid>
-                    <div class="w-full h-full relative" style="width: 100%;">
-                        <div class="fixed" style="z-index:999; width:100%;">
-                            <div class="w-full relative flex justify-end -ml-72" style="z-index:999;">
-                                <Transition name="toast">
-                                    <v-alert
-                                        v-model="isAlert"
-                                        icon="mdi-message"
-                                        type="success"
-                                        closable
-                                        elevation="6"
-                                        :rounded="true"
-                                        position="absolute"
-                                        class="w-max"
-                                        style="z-index:999;"
-                                    >
-                                        This is My message
-                                    </v-alert>
-                                </Transition>
-                            </div>
+                <v-container fluid class="">
+                    <div class="w-full h-full relative">
+                        <div class="h-full">
+                            <slot></slot>
                         </div>
-                        <slot></slot>
                     </div>
                 </v-container>
-
             </VMain>
         </VApp>
     </VThemeProvider>
