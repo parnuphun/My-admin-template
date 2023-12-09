e50d957fe73e39d962a49e47c244805f8da76b98
parnuphun <81858246+parnuphun@users.noreply.github.com>
2023-12-09 15:53:19 +0700

    api path for check delete file [error EPERM]

--- Backend/route/route.js
+++ Backend/route/route.js
@@ -3,6 +3,7 @@ const route = express.Router()
 const login_c = require('../controller/login')
 const persons_pd_c = require('../controller/personal_directory')
 const {upload_person_image  } = require('../services/upload')
+const delete_image = require('../services/delete_image')
 
 // login
 route.post('/login',login_c.login)
@@ -19,4 +20,17 @@ route.post('/addPerson',upload_person_image,persons_pd_c.addPerson)
 route.post('/editPerson',upload_person_image,persons_pd_c.editPerson)
 route.post('/deletePerson',persons_pd_c.deletePerson)
 
+
+route.post('/test_delete_image',(req,res)=>{
+    let dl_state = delete_image(req.body.image_name , 'persons_image')
+    if(dl_state.status){
+        res.json({
+            msg:dl_state.msg
+        })
+    }else{
+        res.json({
+            msg:dl_state.msg
+        })
+    }
+})
 module.exports = route;
