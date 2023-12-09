eef684fa0802a63bd999ce1344c27c581c8b182f
Martinnes2017 <119397950+Martinnes2017@users.noreply.github.com>
2023-12-09 22:14:49 +0700

    ืnewadmin

--- src/components/layout/AdminNavigationBar.vue
+++ src/components/layout/AdminNavigationBar.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { onMounted, reactive, ref , watch ,computed } from 'vue';
-import { navigationMenu , NavigationItem , SubNavigationItem } from '../../plugin/routeData';
-import { useRouter , useRoute} from 'vue-router';
-import { webSetting , layOutTheme } from '../../store/theme/themeData'
+import { onMounted, reactive, ref, watch, computed } from 'vue';
+import { navigationMenu, NavigationItem, SubNavigationItem } from '../../plugin/routeData';
+import { useRouter, useRoute } from 'vue-router';
+import { webSetting, layOutTheme } from '../../store/theme/themeData'
 import { isRail } from '../../store/GlobalData'
 import MsgAlert from '../../services/msgAlert';
 // import { checkPermission , Permission } from "../../services/auth"
@@ -12,36 +12,36 @@ const isOpenMenu = ref(false)
 const isDrawer = ref(true)
 const credentialData = ref()
 const isAlert = ref(false)
-const listOpend =ref<Array<string>>([''])
+const listOpend = ref<Array<string>>([''])
 // theme
 const rentTheme = ref('dark')
-let setting : webSetting = reactive({
-    theme : 'light'
+let setting: webSetting = reactive({
+    theme: 'light'
 })
 
-onMounted(()=>{
+onMounted(() => {
     isGroupOpen()
     // credentialData.value = JSON.parse(localStorage.getItem('credential')||'')
 })
 
-function changeTheme(theme:layOutTheme){
+function changeTheme(theme: layOutTheme) {
     setting.theme = theme
     rentTheme.value = setting.theme
-    localStorage.setItem('setting',JSON.stringify(setting))
+    localStorage.setItem('setting', JSON.stringify(setting))
 }
 
 //route
 const router = useRouter()
 const currentPath = useRoute()
 
-function getCurrentPath(url:string){
+function getCurrentPath(url: string) {
     router.push(url)
 }
 
-function logout(){
-    _msg.confirm('คุณต้องการจากระบบใช่ไหม ?','question').then((isConfirmed)=>{
-        if(isConfirmed){
-            _msg.default_msg({title:'ออกจากระบบแล้ว',timer:1,progressbar:true})
+function logout() {
+    _msg.confirm('คุณต้องการจากระบบใช่ไหม ?', 'question').then((isConfirmed) => {
+        if (isConfirmed) {
+            _msg.default_msg({ title: 'ออกจากระบบแล้ว', timer: 1, progressbar: true })
             localStorage.removeItem('credential')
             setTimeout(() => {
                 router.push('/testBackend/login')
@@ -51,19 +51,19 @@ function logout(){
 }
 
 const isFullScreen = ref(false)
-function fullscreen(){
+function fullscreen() {
     const doc = document.documentElement as HTMLHtmlElement;
-    if(isFullScreen.value === false){
+    if (isFullScreen.value === false) {
         doc.requestFullscreen();
         isFullScreen.value = true
-    }else{
+    } else {
         document.exitFullscreen();
         isFullScreen.value = false
     }
 }
 
-watch(isAlert,()=>{
-    if(isAlert.value) {
+watch(isAlert, () => {
+    if (isAlert.value) {
         setTimeout(() => {
             isAlert.value = false
         }, 5000);
@@ -71,25 +71,25 @@ watch(isAlert,()=>{
 })
 
 const props = defineProps<{
-    isLoadingProgressBar?:boolean
+    isLoadingProgressBar?: boolean
 }>()
 
-function isAdmin():boolean{
+function isAdmin(): boolean {
     console.log('isAdmin fucntion ');
 
     const credentialData = JSON.parse(localStorage.getItem('credential')!)
-    const roles:Array<string> = credentialData.userRoles
+    const roles: Array<string> = credentialData.userRoles
     console.log('before return isAdmin fucntion ');
 
     return roles.some(role => role === 'ผู้ดูแลระบบ');
 }
 
 function isGroupOpen() {
-    for(let i = 0 ; i <= navigationMenu.length ; i++){
-        if(navigationMenu[i] && navigationMenu[i].childs){
-            let listGroup:any = navigationMenu[i].childs
-            for(let j = 0 ; j < navigationMenu[i].childs!.length ; j++ ){
-                if(currentPath.path === listGroup[j].link){
+    for (let i = 0; i <= navigationMenu.length; i++) {
+        if (navigationMenu[i] && navigationMenu[i].childs) {
+            let listGroup: any = navigationMenu[i].childs
+            for (let j = 0; j < navigationMenu[i].childs!.length; j++) {
+                if (currentPath.path === listGroup[j].link) {
                     listOpend.value.length = 0
                     listOpend.value.push(String(navigationMenu[i].id))
                 }
@@ -101,12 +101,12 @@ function isGroupOpen() {
 
 <template>
     <VThemeProvider>
-        <VApp >
-            <div class="h-screen bg-gray-100">
+        <VApp>
+            <div class="h-screen bg-pink-100">
                 <!-- nav bar -->
-                <v-app-bar :elevation="2" class="h-[65px] max-h-[65px] min-h-[65px]">
+                <v-app-bar :elevation="2" class="h-[65px] max-h-[65px] min-h-[65px] bg-pink">
                     <!-- toggle sidebar -->
-                    <v-app-bar-nav-icon  @click.stop="isRail = !isRail"></v-app-bar-nav-icon>
+                    <v-app-bar-nav-icon @click.stop="isRail = !isRail"></v-app-bar-nav-icon>
 
                     <!-- content in nav -->
                     <div class="w-full mr-6 flex flex-row items-center">
@@ -120,7 +120,7 @@ function isGroupOpen() {
                         <!-- user profile -->
                         <v-menu :close-on-content-click="false" location="bottom">
                             <template v-slot:activator="{ props }">
-                                <v-btn  v-bind="props" icon>
+                                <v-btn v-bind="props" icon>
                                     <!-- icon -->
                                     <v-icon size="large">mdi-account-circle-outline</v-icon>
                                 </v-btn>
@@ -128,25 +128,16 @@ function isGroupOpen() {
 
                             <v-card min-width="300">
                                 <v-list line="three">
-                                    <v-list-item
-                                        :title="`${credentialData.userFname} ${credentialData.userLname}`"
-                                        :subtitle="credentialData.userRoles.toString()"
-                                    >
+                                    <v-list-item :title="`${credentialData.userFname} ${credentialData.userLname}`"
+                                        :subtitle="credentialData.userRoles.toString()">
                                         <template v-slot:prepend>
                                             <v-avatar size="x-large">
-                                                <v-img
-                                                    :src="credentialData.userAvatar"
-                                                    alt="user avatar"
-                                                ></v-img>
+                                                <v-img :src="credentialData.userAvatar" alt="user avatar"></v-img>
                                             </v-avatar>
                                         </template>
 
                                         <template v-slot:append>
-                                            <v-btn
-                                                @click="logout()"
-                                                variant="text"
-                                                icon="mdi-login"
-                                            ></v-btn>
+                                            <v-btn @click="logout()" variant="text" icon="mdi-login"></v-btn>
                                         </template>
                                     </v-list-item>
                                 </v-list>
@@ -168,51 +159,28 @@ function isGroupOpen() {
                 </v-progress-linear> -->
 
                 <!-- side bar -->
-                <v-navigation-drawer
-                    v-model="isRail"
-                    :elevation="2"
-                >
+                <v-navigation-drawer v-model="isRail" :elevation="2">
                     <!-- menu list -->
-                    <v-list class="" nav :opened="listOpend" >
+                    <v-list class="" nav :opened="listOpend">
                         <div v-for="navItem of navigationMenu">
                             <!-- v-if=" checkPermission(navItem.permission) || isAdmin() " -->
-                            <v-list-item
-                                v-if="!navItem.childs"
-                                :title="navItem.title"
-                                :subtitle="navItem.subtitle"
-                                @click="getCurrentPath(navItem.link!)"
-                                density="comfortable"
-                                :value="navItem.id"
-                                class="text-md my-1"
-                                active-color=""
-                                :prepend-icon="navItem.icon"
+                            <v-list-item v-if="!navItem.childs" :title="navItem.title" :subtitle="navItem.subtitle"
+                                @click="getCurrentPath(navItem.link!)" density="comfortable" :value="navItem.id"
+                                class="text-md my-1" active-color="" :prepend-icon="navItem.icon"
                                 :active="currentPath.path === navItem.link || (navItem.link === '/dashBoard' && currentPath.path === '/')"
                                 rounded="">
                                 <!-- <v-icon :icon="navItem.icon" start></v-icon>{{navItem.title}} -->
                             </v-list-item>
                             <v-list-group v-else :value="navItem.id">
-                                <template v-slot:activator="{props}">
-                                    <v-list-item
-                                        v-bind="props"
-                                        :title="navItem.title"
-                                        :subtitle="navItem.subtitle"
-                                        @click=""
-                                        density="comfortable"
-                                        :value="navItem.id"
-                                        class="text-md my-1"
-                                        active-color=""
-                                        :prepend-icon="navItem.icon"
-                                    ></v-list-item>
+                                <template v-slot:activator="{ props }">
+                                    <v-list-item v-bind="props" :title="navItem.title" :subtitle="navItem.subtitle"
+                                        @click="" density="comfortable" :value="navItem.id" class="text-md my-1"
+                                        active-color="" :prepend-icon="navItem.icon"></v-list-item>
                                 </template>
                                 <div v-for="SubNavItem of navItem.childs">
-                                    <v-list-item
-                                        :title="SubNavItem.title"
-                                        :subtitle="SubNavItem.subtitle"
-                                        @click="getCurrentPath(SubNavItem.link!)"
-                                        density="comfortable"
-                                        :value="SubNavItem.id"
-                                        class="text-md my-1"
-                                        active-color=""
+                                    <v-list-item :title="SubNavItem.title" :subtitle="SubNavItem.subtitle"
+                                        @click="getCurrentPath(SubNavItem.link!)" density="comfortable"
+                                        :value="SubNavItem.id" class="text-md my-1" active-color=""
                                         :active="currentPath.path === SubNavItem.link || (SubNavItem.link === '/dashBoard' && currentPath.path === '/')"
                                         rounded="">
                                         <template v-slot:prepend v-if="SubNavItem.icon">
@@ -225,12 +193,12 @@ function isGroupOpen() {
                     </v-list>
                 </v-navigation-drawer>
 
-                
+
                 <!-- Content here !!! -->
                 <VMain class="h-full">
                     <v-container fluid class="h-full">
                         <div class="w-full h-full overflow-auto ">
-                            <slot ></slot>
+                            <slot></slot>
                         </div>
                     </v-container>
                 </VMain>
@@ -240,28 +208,33 @@ function isGroupOpen() {
 </template>
 
 <style>
-    .toast-enter-from {
-        opacity: 0 ;
-        transform: translateX(600px)
-    }
-    .toast-enter-to {
-        opacity: 1;
-        transform: translateY(0);
-    }
-    .toast-enter-active {
-        transition: all 0.5s ease-in-out;
-    }
+.toast-enter-from {
+    opacity: 0;
+    transform: translateX(600px)
+}
 
-    .toast-leave-from {
-        opacity: 1;
-        transform: translateY(0);
-    }
-    .toast-leave-to {
-        opacity: 0 ;
-        transform: translate(600px)
+.toast-enter-to {
+    opacity: 1;
+    transform: translateY(0);
+}
 
-    }
-    .toast-leave-active {
-        transition: all 0.3s ease-in-out;
-    }
+.toast-enter-active {
+    transition: all 0.5s ease-in-out;
+}
+
+.toast-leave-from {
+    opacity: 1;
+    transform: translateY(0);
+}
+
+.toast-leave-to {
+    opacity: 0;
+    transform: translate(600px)
+}
+
+.toast-leave-active {
+    transition: all 0.3s ease-in-out;
+}
+
+
 </style>
--- src/views/School/Admin/adminManagement.vue
+++ src/views/School/Admin/adminManagement.vue
@@ -7,10 +7,13 @@ onMounted(() => {
 })
 const dialog = ref(false)
 const adminDialogAdd = ref(false)
+const adminEdit = ref(false)
+const adminMoveoutline = ref(false)
+const adminDelete = ref(false)
 const desserts = ref<Array<any>>([
     {
         user: 'user01',
-        password: 159,
+        password: 12345678910,
         fullname: 'sahassawat meekaew',
         email: 'sahassawatneszz@gmail.com',
         phonenumber: '0000000',
@@ -39,86 +42,116 @@ const desserts = ref<Array<any>>([
         user: 'Lollipop',
         password: 392,
     },
+    {
+        user: 'Jelly bean',
+        password: 375,
+    },
+    {
+        user: 'Jelly bean',
+        password: 375,
+    },
+    {
+        user: 'Jelly bean',
+        password: 375,
+    },
+    {
+        user: 'Jelly bean',
+        password: 375,
+    },
+
+
 
 ],)
 </script>
 
 <template>
     <AdminNavigationBar>
-        ผู้ดูแลระบบ
-        <div class="flex flex-col w-full h-full ">
-            <div class="w-full h-full flex flex-wrap ">
-
-                <div class="w-3/2 h-full overflow-auto border-2 border-gray-300">
-                    <v-card class="h-full" color="">
-        <v-btn @click="adminDialogAdd = !adminDialogAdd" color="pink">
-            <v-icon icon="mdi-account"> </v-icon> เพิ่มบุคลากร
-        </v-btn>
-        <v-table>
 
 
-            <thead>
-                <tr>
-                    <th class="text-left">
-                        ภาพ
-                    </th>
+        <div class="flex justify-between mb-4 ml-4 mr-4">
+            <div class="text-2xl">
+                ผู้ดูแลระบบ
+            </div>
 
-                    <th class="text-left">
-                        ชื่อผู้ใช้
-                    </th>
-                    <th class="text-left">
-                        รหัสผ่าน
-                    </th>
-                    <th class="text-left">
-                        ชื่อ-นามสกุล
-                    </th>
-                    <th class="text-left">
-                        อีเมลล์
-                    </th>
-                    <th class="text-left">
-                        เบอร์โทร
-                    </th>
-                    <th class="text-center w-fit">
+            <v-btn @click="adminDialogAdd = !adminDialogAdd" color="pink">
+                <v-icon icon="mdi-account"> </v-icon> เพิ่มบุคลากร
+            </v-btn>
+        </div>
 
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item, i) in desserts" :key="item.name">
-                    <td>
-                        <img class="w-10 h-10 object-cover" :src="item.url" alt="">
-                    </td>
-                    <td>{{ item.user }}</td>
-                    <td>{{ item.password }}</td>
-                    <td>{{ item.fullname }}</td>
-                    <td>{{ item.email }}</td>
-                    <td>{{ item.phonenumber }}</td>
-                    <td>
-                        <div class="w-full flex flex-row gap-2 justify-center">
-                            <div class="text-blue-500 hover:text-blue-600 cursor-pointer">
-                                <v-icon icon="mdi-pencil"></v-icon>
-                            </div>
-                            <div class="text-green-500 hover:text-green-600 cursor-pointer">
-                                <v-icon icon="mdi-folder-move-outline"></v-icon>
-                            </div>
-                            <div class="text-red-500 hover:text-red-600 cursor-pointer">
-                                <v-icon icon="mdi-delete"></v-icon>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
+        <div class="w-full h-96 overflow-auto border-2 border-gray-300">
+            <div>
 
-            </tbody>
+                <v-table>
+                    <thead>
+                        <tr>
+                            <td class="text-left font-bold text-3xl">
+                                ภาพ
+                            </td>
+                            <td class="text-left font-bold text-3xl">
+                                ชื่อผู้ใช้
+                            </td>
+                            <td class="text-left font-bold text-3xl">
+                                รหัสผ่าน
+                            </td>
+                            <td class="text-left font-bold text-3xl">
+                                ชื่อ-นามสกุล
+                            </td>
+                            <td class="text-left font-bold text-3xl">
+                                อีเมลล์
+                            </td>
+                            <td class="text-left font-bold text-3xl">
+                                เบอร์โทร
+                            </td>
+                            <td class="text-center w-fit">
 
-        </v-table>
-    </v-card>
-    </div>
-    </div>
-    </div>
+                            </td>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr v-for="(item, i) in desserts" :key="item.name">
+                            <td>
+                                <img class="w-10 h-10 object-cover" :src="item.url" alt="">
+                            </td>
+                            <td>{{ item.user }}</td>
+                            <td>{{ item.password }}</td>
+                            <td>{{ item.fullname }}</td>
+                            <td>{{ item.email }}</td>
+                            <td>{{ item.phonenumber }}</td>
+                            <td>
+                                <div class="w-full flex flex-row gap-2 justify-center">
+                                    <div class="text-blue-500 hover:text-blue-600 cursor-pointer">
+                                        <v-btn @click="adminEdit = !adminEdit">
+                                            <v-icon
+                                                class="fill-current text-blue-500 hover:text-blue-600">mdi-pencil</v-icon>
+                                        </v-btn>
+                                    </div>
+                                    <div class="text-green-500 hover:text-green-600 cursor-pointer">
+                                        <v-btn @click="adminMoveoutline = !adminMoveoutline">
+                                            <v-icon
+                                                class="fill-current text-green-500 hover:text-green-600">mdi-folder-move-outline</v-icon>
+                                        </v-btn>
+                                    </div>
+                                    <div class="text-red-500 hover:text-red-600 cursor-pointer">
+                                        <v-btn @click="adminDelete = !adminDelete">
+                                            <v-icon class="fill-current text-red-500 hover:text-red-600">mdi-delete</v-icon>
+                                        </v-btn>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </tbody>
+
+                </v-table>
+
+            </div>
+        </div>
+
         <v-pagination :length="4"></v-pagination>
     </AdminNavigationBar>
 
-
+    <!-- Add -->
     <v-dialog v-model="adminDialogAdd" width="800" transition="dialog-bottom-transition">
         <v-card class="pb-2">
             <!-- content -->
@@ -131,23 +164,115 @@ const desserts = ref<Array<any>>([
                     </div>
                 </div>
                 <div class="w-full px-6 pb-4">
-                    <div class="flex flex-row gap-2 w-full">
-                        <tb> <v-text-field label="ภาพ"></v-text-field>
-                            <v-text-field label="ชื่อผู้ใช้"></v-text-field>
-                            <v-text-field label="รหัส"></v-text-field>
-                            <v-text-field label="ชื่อ-นามสกุล"></v-text-field>
-                            <v-text-field label="อีเมลล์"></v-text-field>
-                            <v-text-field label="เบอร์โทร"></v-text-field>
-                        </tb>
+                    <div class="flex flex-col gap-2 w-full mt-4">
+                        <v-text-field label="ภาพ" width="150"></v-text-field>
+                        <v-text-field label="ชื่อผู้ใช้" width="150"></v-text-field>
+                        <v-text-field label="รหัส" width="100"></v-text-field>
+                        <v-text-field label="ชื่อ-นามสกุล" width="250"></v-text-field>
+                        <v-text-field label="อีเมลล์" width="200"></v-text-field>
+                        <v-text-field label="เบอร์โทร" width="150"></v-text-field>
                     </div>
-                    <div class="้px-3 w-[100px] ">
-                            <div class="w-full h-[55px] rounded-lg text-xl
+
+                    <div class="px-3 w-[100px] ">
+                        <div class="w-full h-[55px] rounded-lg text-xl
                             bg-blue-500 hover:bg-blue-600 text-white flex justify-center items-center cursor-pointer">
-                                ok
-                            </div>
+                            Ok
                         </div>
+                    </div>
                 </div>
             </div>
+        </v-card>
+    </v-dialog>
+    <!-- Edit -->
+    <v-dialog v-model="adminEdit" width="800" transition="dialog-bottom-transition">
+        <v-card class="pb-2">
+            <!-- content -->
+            <div class="flex flex-col w-full ">
+                <div class="w-full py-3 flex justify-center text-2xl mt-3 relative">
+                    แก้ไข
+                    <div @click="adminEdit = !adminEdit"
+                        class="top-2 right-2 absolute h-10 w-10 text-red-500 hover:text-red-600 cursor-pointer text-2xl">
+                        <v-icon icon="mdi-close"></v-icon>
+                    </div>
+                </div>
+                <div class="w-full px-6 pb-4">
+                    <div class="flex flex-col gap-2 w-full mt-4">
+                        <v-text-field class="relative">
+                            <label class="absolute top-0 left-0 mt-2 ml-2">
+                                <img src="your_image_url" alt="ภาพ" class="w-4 h-4">
+                            </label>
+                        </v-text-field>
+                        <v-text-field label="ชื่อผู้ใช้" width="150"></v-text-field>
+                        <v-text-field label="รหัส" width="100"></v-text-field>
+                        <v-text-field label="ชื่อ-นามสกุล" width="250"></v-text-field>
+                        <v-text-field label="อีเมลล์" width="200"></v-text-field>
+                        <v-text-field label="เบอร์โทร" width="150"></v-text-field>
+                    </div>
+
+                    <div class="mx-auto  w-[100px] ">
+                        <div class="w-full h-[55px] rounded-lg text-xl
+                            bg-blue-500 hover:bg-blue-600 text-white flex justify-center items-center cursor-pointer">
+                            ตกลง
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="adminMoveoutline" width="800" transition="dialog-bottom-transition">
+        <v-card class="pb-2">
+            <!-- content -->
+            <div class="flex flex-col w-full ">
+                <div class="w-full py-3 flex justify-center text-2xl mt-3 relative">
+                    move outline
+                    <div @click="adminMoveoutline = !adminMoveoutline"
+                        class="top-2 right-2 absolute h-10 w-10 text-red-500 hover:text-red-600 cursor-pointer text-2xl">
+                        <v-icon icon="mdi-close"></v-icon>
+                    </div>
+                </div>
+                <div class="w-full px-6 pb-4">
+                    <div class="flex flex-col gap-2 w-full mt-4">
+                        <v-text-field label="ภาพ" width="200"></v-text-field>
+                        <v-text-field label="ชื่อผู้ใช้" width="150"></v-text-field>
+                        <v-text-field label="รหัส" width="100"></v-text-field>
+                        <v-text-field label="ชื่อ-นามสกุล" width="250"></v-text-field>
+                        <v-text-field label="อีเมลล์" width="200"></v-text-field>
+                        <v-text-field label="เบอร์โทร" width="150"></v-text-field>
+                    </div>
+
+                    <div class="mx-auto  w-[100px] ">
+                        <div class="w-full h-[55px] rounded-lg text-xl
+                            bg-blue-500 hover:bg-blue-600 text-white flex justify-center items-center cursor-pointer">
+                            ตกลง
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </v-card>
+    </v-dialog>
+
+    <!-- delete -->
+    <v-dialog class="mx-auto" v-model="adminDelete" width="800" transition="dialog-bottom-transition">
+        <v-card class=" pb-15">
+            <!-- content -->
+            <div class=" flex flex-col w-full ">
+                <div class="w-full py-6 flex justify-center text-2xl mt-4 mb-2 relative">
+                    ต้องการลบ
+                    <div @click="adminDelete = !adminDelete"
+                        class="top-2 right-2 absolute h-10 w-10 text-red-500 hover:text-red-600 cursor-pointer text-2xl">
+                        <v-icon icon="mdi-close"></v-icon>
+                    </div>
+                </div>
+                <div class="mx-auto   w-[100px] ">
+
+                    <div
+                        class=" flex items-center justify-center px-3 w-[100px] h-[55px] rounded-lg text-xl bg-blue-500 hover:bg-blue-600 text-white cursor-pointer">
+                        ลบ
+                    </div>
+                </div>
+            </div>
+
         </v-card>
     </v-dialog>
 </template>
