e19b08085a246923d42694cac4a317d0b34d1d47
parnuphun <81858246+parnuphun@users.noreply.github.com>
2023-12-14 11:00:50 +0700

    comment

--- src/store/Interface.ts
+++ src/store/Interface.ts
@@ -25,46 +25,46 @@ export interface imageGalleryType {
 
 // old
 
-type userRoles = 'อาจารย์' | 'นักศึกษา' | 'ผู้ดูแลระบบ'
+// type userRoles = 'อาจารย์' | 'นักศึกษา' | 'ผู้ดูแลระบบ'
 
-export interface CredentialDataFromLocal {
-    Authorization: string
-    userAvatar: string
-    userEmail: string
-    userFname: string
-    userLname: string
-    userId: number
-    userRmutiId: string
-    userRoles: userRoles[]
-}
+// export interface CredentialDataFromLocal {
+//     Authorization: string
+//     userAvatar: string
+//     userEmail: string
+//     userFname: string
+//     userLname: string
+//     userId: number
+//     userRmutiId: string
+//     userRoles: userRoles[]
+// }
 
-export interface userDataRes {
-    User_Id: number
-    User_Avatar: string
-    User_Usernname: string
-    User_Email: string
-    User_Rmuti_Id: string
-    User_password: string
-    User_Fname: string
-    User_Lname: string
-    User_Phone: string
-    User_AboutMe: string
-    User_Joined_Date: Date
-    User_Email_Confirm: boolean
-    User_Deleted: boolean
-    User_Banned: boolean
-    USER_ROLES: string[]
-}
+// export interface userDataRes {
+//     User_Id: number
+//     User_Avatar: string
+//     User_Usernname: string
+//     User_Email: string
+//     User_Rmuti_Id: string
+//     User_password: string
+//     User_Fname: string
+//     User_Lname: string
+//     User_Phone: string
+//     User_AboutMe: string
+//     User_Joined_Date: Date
+//     User_Email_Confirm: boolean
+//     User_Deleted: boolean
+//     User_Banned: boolean
+//     USER_ROLES: string[]
+// }
 
-export interface ProjectDataRes {
-    Project_Id: number
-    Project_Name_TH: string
-    Project_Name_EN: string
-    Project_Detail: string
-    Project_Avatar: string
-    Project_Status: { Project_Status_Id: number, Project_Status_Name: string }
-    Project_Created_By: number // user
-    Project_Created_Date: Date
-    Project_Deleted: boolean
-}
+// export interface ProjectDataRes {
+//     Project_Id: number
+//     Project_Name_TH: string
+//     Project_Name_EN: string
+//     Project_Detail: string
+//     Project_Avatar: string
+//     Project_Status: { Project_Status_Id: number, Project_Status_Name: string }
+//     Project_Created_By: number // user
+//     Project_Created_Date: Date
+//     Project_Deleted: boolean
+// }
 
