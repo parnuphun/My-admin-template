d616bfda21d3df23743d505c4da9c57ff9d49610
parnuphun <81858246+parnuphun@users.noreply.github.com>
2023-12-19 19:19:43 +0700

    use default breakpoint tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,21 +9,21 @@ module.exports = {
     theme: {
         // default breakpoint
         screens: {
-            // 'sm': '640px',
-            // 'md': '768px',
-            // 'lg': '1024px',
-            // 'xl': '1280px',
-            // '2xl': '1536px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
           }
     },
     extend: {
         // add breakpoint
         screens: {
             'less' : '100px',
-            'MB' : '200px',
-            'TL' : '640px',
-            'LT': '1024px',
-            'DT' : '1536px',
+            // 'MB' : '200px',
+            // 'TL' : '640px',
+            // 'LT': '1024px',
+            // 'DT' : '1536px',
 
         }
     },
