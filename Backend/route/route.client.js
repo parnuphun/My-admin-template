cd8a987c6a7d2cf0539124205650cbd1149dc1b7
parnuphun <81858246+parnuphun@users.noreply.github.com>
2024-01-23 23:38:45 +0700

    update comment client route

--- Backend/route/route.client.js
+++ Backend/route/route.client.js
@@ -4,19 +4,19 @@ const db = require('../config/database');
 
 const client_c = require('../controller/client')
 
-route.post('/clientMainPage',client_c.mainPage)
-route.post('/clientGetNewsContent',client_c.getNewsContent)
-route.post('/getNewsListClient',client_c.getNewsListClient)
-route.post('/getNewsLengthClient',client_c.getNewsLengthClient)
-route.post('/getActivityImageListClient',client_c.getActivityImageListClient)
-route.post('/getActivityImageLengthClient',client_c.getActivityImageLengthClient)
-route.post('/getAllfilsClient',client_c.getAllfilsClient)
-route.post('/previewFileClient',client_c.previewFileClient)
-route.post('/getPersonTreeclient',client_c.getPersonTreeclient)
-route.post('/getPersonCateListClient',client_c.getPersonCateListClient)
-route.post('/getTeachingSClient',client_c.getTeachingSClient)
-route.post('/getStudentSClient',client_c.getStudentSClient)
-route.post('/getClassClient',client_c.getClassClient)
-route.post('/getSyllabusClient',client_c.getSyllabusClient)
+route.post('/clientMainPage',client_c.mainPage) // ดึงข้อมูลไปยังหน้าแรกของเว็บไซ์
+route.post('/clientGetNewsContent',client_c.getNewsContent) // ดึงข้อมูลรายละเอียดข่าวสารไปแสดง
+route.post('/getNewsListClient',client_c.getNewsListClient) // ดึงข้อมูลข่าวสารไปยังหน้าข่าวสาร
+route.post('/getNewsLengthClient',client_c.getNewsLengthClient) // ดึงจำนวนข้อมูลข่าวสาร
+route.post('/getActivityImageListClient',client_c.getActivityImageListClient) // ดึงข้อมูลรายการภาพกิจกรรมไปยังหน้าภาพกิจกรรม
+route.post('/getActivityImageLengthClient',client_c.getActivityImageLengthClient) // ดึงจำนววนข้อมูลภาพกิจกรรม
+route.post('/getAllfilsClient',client_c.getAllfilsClient) // ดึงไฟล์เอกสารไปยังหน้าเอกสาร
+route.post('/previewFileClient',client_c.previewFileClient) // ดูรายละเอียดไฟล์เอกสาร 
+route.post('/getPersonTreeclient',client_c.getPersonTreeclient) // ดึงรายการทำเนียบบุคลากร
+route.post('/getPersonCateListClient',client_c.getPersonCateListClient) // ดึงประเภทรายการทำเนียบบุคลากร
+route.post('/getTeachingSClient',client_c.getTeachingSClient) // ดึงข้อมูลรายการตารางสอน
+route.post('/getStudentSClient',client_c.getStudentSClient) // ดึงข้อมูลรายการตารางเรียน
+route.post('/getClassClient',client_c.getClassClient) // ดึงรายชื่อชั้นเรียน
+route.post('/getSyllabusClient',client_c.getSyllabusClient) // ดึงรายการหลักสูตร
 
 module.exports = route
